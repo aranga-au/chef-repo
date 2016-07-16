c5817a24fbab6253675b3372e2f64448bd4290fa
aranga-au <aranga.nanayakkara@gmail.com>
2016-07-16 11:02:38 +1000

    Burkefile changes

---
+++ cookbooks/my_cookbook/metadata.rb
@@ -0,0 +1,14 @@
+name             'my_cookbook'
+maintainer       'Aranga Nanayakkara'
+maintainer_email 'aranga.nanayakkara@gmail.com'
+license          'MIT'
+description      'Installs/Configures my_cookbook'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.0'
+
+
+depends  'build-essential'
+depends 'apache2', '>= 1.0.4'
+depends 'chef-client'
+depends 'apt'
+depends 'npt'
